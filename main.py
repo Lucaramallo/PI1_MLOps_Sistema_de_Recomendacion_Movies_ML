ccab77df58b544b17d9104b5e9b67a2b02e50f36
Lucaramallo <95667827+Lucaramallo@users.noreply.github.com>
2023-08-08 09:40:07 -0300

    Recomendation function 7

--- main.py
+++ main.py
@@ -5,11 +5,44 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+@app.get('/peliculas_idioma/{idioma}')
+def peliculas_idioma(idioma:str):
+    '''Ingresas el idioma, retornando la cantidad de peliculas producidas en el mismo'''
+    return {'idioma':idioma, 'cantidad':respuesta}
+    
+ @app.get('/peliculas_duracion/{pelicula}')
+def peliculas_duracion(pelicula:str):
+    '''Ingresas la pelicula, retornando la duracion y el año'''
+    return {'pelicula':pelicula, 'duracion':respuesta, 'anio':anio}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get('/franquicia/{franquicia}')
+def franquicia(franquicia:str):
+    '''Se ingresa la franquicia, retornando la cantidad de peliculas, ganancia total y promedio'''
+    return {'franquicia':franquicia, 'cantidad':respuesta, 'ganancia_total':respuesta, 'ganancia_promedio':respuesta}
+
+@app.get('/peliculas_pais/{pais}')
+def peliculas_pais(pais:str):
+    '''Ingresas el pais, retornando la cantidad de peliculas producidas en el mismo'''
+    return {'pais':pais, 'cantidad':respuesta}
+
+@app.get('/productoras_exitosas/{productora}')
+def productoras_exitosas(productora:str):
+    '''Ingresas la productora, entregandote el revunue total y la cantidad de peliculas que realizo '''
+    return {'productora':productora, 'revenue_total': respuesta,'cantidad':respuesta}
+
+
+@app.get('/get_director/{nombre_director}')
+def get_director(nombre_director:str):
+    ''' Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. 
+    Además, deberá devolver el nombre de cada película con la fecha de lanzamiento, retorno individual, costo y ganancia de la misma. En formato lista'''
+    return {'director':nombre_director, 'retorno_total_director':respuesta, 
+    'peliculas':respuesta, 'anio':respuesta,, 'retorno_pelicula':respuesta, 
+    'budget_pelicula':respuesta, 'revenue_pelicula':respuesta}
+
+# ML
+@app.get('/recomendacion/{titulo}')
+def recomendacion(titulo:str):
+    '''Ingresas un nombre de pelicula y te recomienda las similares en una lista'''
+    return {'lista recomendada': respuesta}
